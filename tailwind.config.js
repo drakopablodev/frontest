5eef0b3eac584295ccec53ab1e93b020add6bd35
drakopablo.dev <drakopablo.dev@gmail.com>
2023-09-05 14:12:28 +0200

    yet i uploaded another update

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,7 +19,12 @@ module.exports = {
     theme: {
       /* Extensiones de variables */
       extend: {
+          borderRadius: {
+              '4xl': '18px',
+              '5xl': '20px',
+          },
           colors: {     
+            'background': '#FFFFFF',
             primary: {"50":"#eff6ff","100":"#dbeafe","200":"#bfdbfe","300":"#93c5fd","400":"#60a5fa","500":"#3b82f6","600":"#2563eb","700":"#1d4ed8","800":"#1e40af","900":"#1e3a8a","950":"#172554"},
             /* Konetic colors */
             'konetic': {
@@ -66,6 +71,18 @@ module.exports = {
             '7.5': '30px',
             '8.5': '34px',
             '9.5': '38px'
+          },
+          screens: {
+            'sm': '640px',
+            // => @media (min-width: 640px) { ... }      
+            'md': '768px',
+            // => @media (min-width: 768px) { ... }      
+            'lg': '1024px',
+            // => @media (min-width: 1024px) { ... }      
+            'xl': '1280px',
+            // => @media (min-width: 1280px) { ... }      
+            '2xl': '1536px',
+            // => @media (min-width: 1536px) { ... }
           },
           transform: ['hover', 'focus', 'active', 'disabled'],
           transitionDuration : {
